18998e6b68cbd53da3c15887fb2e71fee0786a42
Manuel <mln204manu@gmail.com>
2025-05-12 23:37:56 -0300

    backend

---
+++ BUEN_SABOR_BACKEND/src/test/java/com/lab4/buen_sabor_backend/BuenSaborBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab4.buen_sabor_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuenSaborBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
