696e1458539b0e076061321a8fbdfe9865bdf662
LucasGonzalez <topolucasnicolas@gmail.com>
2025-05-27 11:44:08 -0300

    Creacion de CRUD de ImagenArticuloController y DetalleArticuloManufacturadoController

---
+++ BUEN_SABOR_BACKEND/src/main/java/com/lab4/buen_sabor_backend/controller/DetalleArticuloManufacturadoController.java
@@ -0,0 +1,40 @@
+package com.lab4.buen_sabor_backend.controller;
+
+import com.lab4.buen_sabor_backend.dto.DetalleArticuloManufacturadoDTO;
+import com.lab4.buen_sabor_backend.mapper.DetalleArticuloManufacturadoMapper;
+import com.lab4.buen_sabor_backend.model.DetalleArticuloManufacturado;
+import com.lab4.buen_sabor_backend.service.DetalleArticuloManufacturadoService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/detalleArticulo")
+@CrossOrigin(origins = "*")
+public class DetalleArticuloManufacturadoController extends MasterControllerImpl<DetalleArticuloManufacturado, DetalleArticuloManufacturadoDTO, Long> implements MasterController<DetalleArticuloManufacturadoDTO, Long> {
+
+    private static final Logger logger = LoggerFactory.getLogger(DetalleArticuloManufacturadoController.class);
+
+    private final DetalleArticuloManufacturadoService DetalleArticuloManufacturadoService;
+    private final DetalleArticuloManufacturadoMapper DetalleArticuloManufacturadoMapper;
+
+    @Autowired
+    public DetalleArticuloManufacturadoController(DetalleArticuloManufacturadoService DetalleArticuloManufacturadoService, DetalleArticuloManufacturadoMapper DetalleArticuloManufacturadoMapper) {
+        super(DetalleArticuloManufacturadoService);
+        this.DetalleArticuloManufacturadoService = DetalleArticuloManufacturadoService;
+        this.DetalleArticuloManufacturadoMapper = DetalleArticuloManufacturadoMapper;
+    }
+
+    @Override
+    protected DetalleArticuloManufacturado toEntity(DetalleArticuloManufacturadoDTO dto) {
+        return DetalleArticuloManufacturadoMapper.toEntity(dto);
+    }
+
+    @Override
+    protected DetalleArticuloManufacturadoDTO toDTO(DetalleArticuloManufacturado entity) {
+        return DetalleArticuloManufacturadoMapper.toDTO(entity);
+    }
+}
