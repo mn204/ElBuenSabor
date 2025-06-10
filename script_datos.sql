16a44f42125522b466300190a006b4bde5205e02
Manuel <mln204manu@gmail.com>
2025-06-10 01:19:42 -0300

    actualizaciones del script

--- script_datos.sql
+++ script_datos.sql
@@ -50,38 +50,21 @@ INSERT INTO LOCALIDAD (ELIMINADO, NOMBRE, PROVINCIA_ID) VALUES
                                                             (0, 'Junín', @mendoza_provincia_id),
                                                             (0, 'La Paz', @mendoza_provincia_id);
 -- USUARIO
-INSERT INTO USUARIO VALUES
-                                   (1, FALSE, '123123', 'admin@buensa.com', '0wT1WSonPZfdiXIoceu2imy7GJ53', 'https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749221525/jhzs2z2iod6vznfirkv3.jpg',  'password', 'ADMINISTRADOR'),
-                                   (2, FALSE, '123', 'cajero@buensa.com', 'IvLNrda6CkPiT7uqp3DfpguF6rZ2','https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749221580/hirlalizap8uluw3sa1u.jpg',  'password', 'CAJERO'),
-                                   (3, FALSE, '1234', 'cocinero@buensa.com', 'FBOrztuIpidGMVcJRRmsWDfbNf83','https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749221678/pkfdcanbungp7lpx75xo.jpg',  'password', 'COCINERO'),
-                                   (4, FALSE, '12345', 'delivery@buensa.com', 'aqi54XpX0wgvG3jhIkSSukDg7Yx2','https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749221617/l42s8t31mivewaalmmuq.jpg',  'password', 'DELIVERY'),
-                                   (5, FALSE, '4334343', 'juan@hotmail.com', 'v1E2gY1G90MlcrqT3jefJBSMVhf1','https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749220817/yekjwtrrthcqf3thhu3c.jpg',  'password', 'CLIENTE'),
-                                    (6,FALSE,'12121242','mln204manutup@gmail.com','70oXKoe8CyePy3dFYy7P9SRjGwJ3','https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749221525/jhzs2z2iod6vznfirkv3.jpg', 'password','ADMINISTRADOR');
-
-
+insert  into `usuario`(`id`,`eliminado`,`dni`,`email`,`firebase_uid`,`photo_url`,`provider_id`,`rol`) values (1,'\0','123123','admin@buensa.com','0wT1WSonPZfdiXIoceu2imy7GJ53','https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749221525/jhzs2z2iod6vznfirkv3.jpg','password','ADMINISTRADOR'),(2,'\0','123','cajero@buensa.com','IvLNrda6CkPiT7uqp3DfpguF6rZ2','https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749221580/hirlalizap8uluw3sa1u.jpg','password','CAJERO'),(3,'\0','1234','cocinero@buensa.com','FBOrztuIpidGMVcJRRmsWDfbNf83','https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749221678/pkfdcanbungp7lpx75xo.jpg','password','COCINERO'),(4,'\0','12345','delivery@buensa.com','aqi54XpX0wgvG3jhIkSSukDg7Yx2','https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749221617/l42s8t31mivewaalmmuq.jpg','password','DELIVERY'),(5,'\0','4334343','juan@hotmail.com','v1E2gY1G90MlcrqT3jefJBSMVhf1','https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749220817/yekjwtrrthcqf3thhu3c.jpg','password','CLIENTE'),(6,'\0','12121242','mln204manutup@gmail.com','70oXKoe8CyePy3dFYy7P9SRjGwJ3','https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749221525/jhzs2z2iod6vznfirkv3.jpg','password','ADMINISTRADOR'),(7,'\0','4223232','luis@hotmail.com','h8stw8x2WJYlcPTj7MuC56YmeNE3','https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749528593/owc3hsdwd921vcsi7omj.jpg','password','CLIENTE'),(8,'\0','24124122','max@hotmail.com','0AqJQjapRXYZ2LmX95XeXFib9ev2','https://res.cloudinary.com/dvyjtb1ns/image/upload/v1749528768/si6msmbtkcefciwl67nc.jpg','password','CLIENTE');
 
 -- DOMICILIO (al menos 4)
-INSERT INTO DOMICILIO VALUES
-                                     (1, FALSE, 'Baez', 5500, 'detalles', '23', 209, '1', 3),
-                                     (2, FALSE, 'Lar', 5503, 'depto', '', 122, '', 2),
-                                     (3, FALSE, 'San Martín', 5515, 'casa', NULL, 154, NULL, 5),
-                                     (4, FALSE, 'Belgrano', 5501, NULL, 'B', 301, '3', 7),
-                                    (5, FALSE,'Peru',5500,'Casa',NULL,123,'',1);
+insert  into `domicilio`(`id`,`eliminado`,`calle`,`codigo_postal`,`detalles`,`nro_departamento`,`numero`,`piso`,`localidad_id`) values (1,'\0','Baez',5500,'detalles','23',209,'1',3),(2,'\0','Lar',5503,'depto','',122,'',2),(3,'\0','San Martín',5515,'casa',NULL,154,NULL,5),(4,'\0','Belgrano',5501,NULL,'B',301,'3',7),(5,'\0','Peru',5500,'Casa',NULL,123,'',1),(6,'\0','Chile',5500,'Casa',NULL,209,'',1),(7,'\0','Mitre',5500,'Depto','26',300,'1',1),(8,'\0','Massa',5508,'Casa vieja',NULL,232,'',4),(9,'\0','Rivadavia',5502,'Depto',NULL,208,'1',2);
 
 -- EMPLEADO
 INSERT INTO EMPLEADO VALUES
-                                    (1, FALSE, 'Gómez', '1990-05-12', 'Ana', '2611111111', 1, 1),
-                                    (2, FALSE, 'Pérez', '1985-03-20', 'Luis', '2612222222', 2, 2),
-                                    (3, FALSE, 'Rodríguez', '1992-08-30', 'Marta', '2613333333', 3, 3),
-                                    (4, FALSE, 'Fernández', '1995-11-11', 'Carlos', '2614444444', 4, 4),
-                                    (5, FALSE,  'Diaz','2009-02-11','Franco','124115',5, 6);
+                         (1, FALSE, 'Gómez', '1990-05-12', 'Ana', '2611111111', 1, 1),
+                         (2, FALSE, 'Pérez', '1985-03-20', 'Luis', '2612222222', 2, 2),
+                         (3, FALSE, 'Rodríguez', '1992-08-30', 'Marta', '2613333333', 3, 3),
+                         (4, FALSE, 'Fernández', '1995-11-11', 'Carlos', '2614444444', 4, 4),
+                         (5, FALSE,  'Diaz','2009-02-11','Franco','124115',5, 6);
 
 -- CLIENTE
-INSERT INTO CLIENTE VALUES
-    (1, FALSE, 'Ramírez', '2000-01-01', 'Juan', '2615555555', 5);
+insert  into `cliente`(`id`,`eliminado`,`apellido`,`fecha_nacimiento`,`nombre`,`telefono`,`usuario_id`) values (1,'\0','Ramírez','2000-01-01','Juan','2615555555',5),(2,'\0','Diz','1999-06-09','Luis','121233223213',7),(3,'\0','Power','2000-02-03','Max','214214224',8);
 
 -- CLIENTE_DOMICILIO (cliente con múltiples domicilios)
-INSERT INTO CLIENTE_DOMICILIO VALUES
-                                             (1, 1),
-                                             (1, 2);
-
+insert  into `cliente_domicilio`(`cliente_id`,`domicilio_id`) values (1,6),(1,7),(2,8),(3,9);
