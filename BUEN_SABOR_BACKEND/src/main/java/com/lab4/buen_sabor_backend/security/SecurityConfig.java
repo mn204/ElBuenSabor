b9cbb6c50e32984741f5a997515cee6e81c93c34
LucasGonzalez <topolucasnicolas@gmail.com>
2025-07-03 01:19:57 -0300

    .

--- BUEN_SABOR_BACKEND/src/main/java/com/lab4/buen_sabor_backend/security/SecurityConfig.java
+++ BUEN_SABOR_BACKEND/src/main/java/com/lab4/buen_sabor_backend/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.lab4.buen_sabor_backend.config;
+package com.lab4.buen_sabor_backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
